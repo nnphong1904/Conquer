9c90c4a6f411fa08388344b13b525d5de2370087
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-07-19 02:50:22 +0700

    Fixed button clicked state of navbar

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 var navLink=document.getElementsByClassName('nav-link');
 var linkArr=[];
+var currentIndex=-1;
 for (var i=0;i<navLink.length;i++){
     linkArr.push(navLink[i]);
 }
-console.log(linkArr);
-
 for (var i=0;i<linkArr.length;i++){
     linkArr[i].addEventListener('click',function(index){
-        console.log(index);
+        var tmp=linkArr.indexOf(index.target);
+        // if (linkArr[tmp].classList.contains('current')==true){
+        //     linkArr[tmp].classList.remove('current');
+        // }
+        // else {
+        //     linkArr[tmp].classList.add('current');
+        // }
+
+        if (currentIndex===tmp){
+            linkArr[tmp].classList.remove('current');
+            currentIndex=-1;
+        }
+        else if (currentIndex!=tmp)
+        {
+            linkArr[tmp].classList.add('current');
+            if (currentIndex!==-1){
+                linkArr[currentIndex].classList.remove('current');
+            }
+            currentIndex=tmp;
+        }
     })
 }
